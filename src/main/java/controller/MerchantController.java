93494fbfdd4cb9c007d8e000a42908f950386ef3
tianyingchun <tianyingchun@outlook.com>
2014-03-31 17:54:28 +0800

    update template files

--- src/main/java/controller/MerchantController.java
+++ src/main/java/controller/MerchantController.java
@@ -1,23 +1,28 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-// @RequestMapping("/merchant/")
+@RequestMapping("/merchant")
 public class MerchantController extends BaseController {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/merchant/index")
+	@RequestMapping(method = RequestMethod.GET, value = "/index")
 	public String index() {
 		return "merchant/index";
 	}
-
-	@ModelAttribute("who")
-	@RequestMapping(method = RequestMethod.GET, value = "/merchant/hello")
-	public String hello() {
-		return "merchant/hello";
+	@RequestMapping(method = RequestMethod.GET, value = "/create")
+	public String creat() {
+		return "merchant/create";
+	}
+	@RequestMapping(method = RequestMethod.GET, value = "/publishedproducts")
+	public String published() {
+		return "merchant/publishedproducts";
+	}
+	@RequestMapping(method = RequestMethod.GET, value = "/deletedproducts")
+	public String deleted() {
+		return "merchant/deletedproducts";
 	}
 
 }
---
+++ src/main/java/controller/OrderController.java
@@ -0,0 +1,13 @@
+package controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/merchant")
+public class OrderController {
+	@RequestMapping(method = RequestMethod.GET, value = "/orderlist")
+	public String orderlist() {
+		return "merchant/orderlist";
+	}
+}	
